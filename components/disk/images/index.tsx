ea535a40cdd1f93e0d5d2737a4fdf39304041074
WeiChen Lin <asdfg09487@gmail.com>
2023-01-11 18:51:29 +0800

    adding bg

---
+++ components/disk/images/index.tsx
@@ -0,0 +1,7 @@
+export default function ImagePlayground() {
+  return (
+    <div className='absolute w-full h-screen bg-slate-700/40 z-30'>
+      <div className='bg-red-100 h-[70%] mt-[5%]'></div>
+    </div>
+  )
+}
